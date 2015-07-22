da894bc0f1bbcc3d71750dfea55643fdbe71d031
Bryant Novas <bryant.novas@gmail.com>
2015-07-22 17:45:13 -0400

    some changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		$("#total").text("Total: 0.00");
 		$(".table-area tbody").empty();
 		$("#cash").text("Cash: 0.00");
-    	$("#change").text("Change: 0.00");
+    $("#change").text("Change: 0.00");
 	});
 	$("#calctip").click(function(){
 		calculateTip();
@@ -44,11 +44,29 @@ $(document).ready(function(){
     		var payment = $(this).val();
     		$("#cash").text("Cash: "+ payment);
     		$("#change").text("Change: " +(payment - total).toFixed(2));
-        	$(this).val("");
+        $(this).val("");
     }
 	});
 });
 
+var prices = [
+	2.00, 
+	1.00, 
+	5.00, 
+	3.28, 
+	2.50, 
+	8.00, 
+	2.28, 
+	20.00, 
+	80.00, 
+	11.00, 
+	17.00, 
+	2.19, 
+	1.89, 
+	12.29, 
+	7.00, 
+	12.00
+]
 var subtotal = 0;
 var tip = 0;
 var total = 0;
@@ -65,88 +83,72 @@ function calulateTotal(){
 function calculateTip() {
 	tip = subtotal * 0.20;
 }
+function appendToDom(item, price) {
+	$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>"+price+"</td> </tr>");
+		subtotal += price;
+	} 
 
 function database(item) {
 	switch (item) {
 	case "orange juice":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>2.00</td> </tr>");
-		subtotal += 2.00;
+		appendToDom(item, prices[0])
 	    break;
 	case "coffee":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>1.00</td> </tr>");
-	 	subtotal += 1.00;
+		appendToDom(item, prices[1])
     	break;
 	case "toast":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>5.00</td> </tr>");
-		subtotal += 5;
+		appendToDom(item, prices[2])
 	break;
 	case "croissant":
-	$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>2.00</td> </tr>");
-		subtotal += 2;
+		appendToDom(item, prices[0])
 	break;
 	case "eggs":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>3.28</td> </tr>");
-		subtotal += 3.28;
+		appendToDom(item, prices[0])
 	break;
 	case "soda":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>1.00</td> </tr>");
-		subtotal+= 1.00;
+		appendToDom(item, prices[1])
 	break;
 	case "pizza":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>2.50</td> </tr>");
-		subtotal += 2.50;
+		appendToDom(item, prices[4])
 	break;
 	case "hot dog":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>8.00</td> </tr>");
-		subtotal += 8;
+		appendToDom(item, prices[5])
 	break;
 	case "fries":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>2.28</td> </tr>");
-		subtotal += 2.28;
+		appendToDom(item, prices[6])
 	break;
 	case "hamburger":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>20.00</td> </tr>");
-		subtotal += 20;
+		appendToDom(item, prices[7])
 	break;
 	case "lobster":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>80.00</td> </tr>");
-		subtotal += 80;
+		appendToDom(item, prices[8])
     break;
 	case "ramen":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>11.00</td> </tr>");
-		subtotal += 11;
+		appendToDom(item, prices[9])
 	break;
 	case "sushi":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>17.00</td> </tr>");
-		subtotal += 17;
+		appendToDom(item, prices[10])
 	break;
 	case "chocolate":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>2.19</td> </tr>");
-		subtotal += 2.19;
+		appendToDom(item, prices[11])
 	break;
 	case "donut":
-	$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>1.89</td> </tr>");
-		subtotal += 1.89;
+		appendToDom(item, prices[12])
 	break;
 	case "cigarretes":
-	$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>12.29</td> </tr>");
-		 subtotal += 12.29;
+		appendToDom(item, prices[13])
 	break;	
 	case "beer":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>7.00</td> </tr>");
-		subtotal += 7;
+		appendToDom(item, prices[14])		
 	break;
 	case "whiskey":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>7.00</td> </tr>");
-		subtotal += 7;
+		appendToDom(item, prices[14])
 	break;
 	case "cocktail":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>12.00</td> </tr>");
-		subtotal += 12;
+		appendToDom(item, prices[15])
 	break;
 	case "wine":
-		$(".table-area tbody ").append("<tr> <td>"+item+"</td> <td>1</td> <td>12.00</td> </tr>");
-		subtotal += 12;
+		appendToDom(item, prices[15])
 	break;
 	default:
 	break;
